604c2a1de0b898643c57ca2f471af50ad4074a58
tanewton0523 <59624833+tanewton0523@users.noreply.github.com>
2025-01-29 11:31:36 -0500

    add port and created db and seed files

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
 
-console.log('MONGO_URI:', process.env.MONGO_URI); // Add this line to debug
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 5000;
 
-mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log(err));
+
+
+
+// mongoose
+//     .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch((err) => console.log(err));
+
+
+// Start server
+app.listen(5000, () => console.log('Server running on port 5000'));

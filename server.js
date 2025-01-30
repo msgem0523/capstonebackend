930b62cdbbc38cf193c4e02af0667676f01d73a1
tanewton0523 <59624833+tanewton0523@users.noreply.github.com>
2025-01-29 23:52:33 -0500

    import routes to server file

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 import dotenv from 'dotenv'; // Import dotenv
 import express from 'express'; // Import express
 import connectDB from './config/db.js'; // Import connectDB function
+import childrenRoutes from './routes/ChildrenRoutes.js'; // Import Children routes
+import medicalRecordsRoutes from './routes/MedicalRecordsRoutes.js'; // Import Medical Records routes
+import milestoneRoutes from './routes/MilestoneRoutes.js'; // Import Milestones routes
+import vaccineRoutes from './routes/VaccineRoutes.js'; // Import Vaccines routes
 
 // Load environment variables
 dotenv.config();
@@ -11,6 +15,12 @@ connectDB();
 
 const app = express(); // Initialize express
 app.use(express.json()); // Middleware for parsing JSON
+
+// Use routes
+app.use('/api/children', childrenRoutes);
+app.use('/api/medical-records', medicalRecordsRoutes);
+app.use('/api/milestones', milestoneRoutes);
+app.use('/api/vaccines', vaccineRoutes);
 
 const PORT = process.env.PORT || 5000; // Port
 // Start server
